9d6543c3905ecc8e6c2b6ad8227e9bb9453261b0
Susisu <susisu2413@yahoo.co.jp>
2017-01-23 02:36:34 +0900

    Clean the code

--- src/Language/Grass/Interpreter/VM.hs
+++ src/Language/Grass/Interpreter/VM.hs
@@ -3,6 +3,11 @@
 
 module Language.Grass.Interpreter.VM
     (
+      Pos (..)
+    , Inst (..)
+    , RuntimeError (..)
+    , Code
+    , run
     ) where
 
 import qualified Data.ByteString as BS
@@ -11,11 +16,24 @@ import Control.Monad.Except
 import Safe
 import System.IO
 
+
+-- instructions
 data Pos = forall p. Show p => Pos p
 
 instance Show Pos where
     show (Pos p) = show p
 
+data Inst  = App Pos Int Int  -- application
+           | Abs Pos Int Code -- abstraction
+
+
+-- values
+data Value = Char Word8               -- character
+           | Prim (Value -> VM Value) -- primitive operation
+           | Clos Env Code            -- closure
+
+
+-- VM
 data RuntimeError = RuntimeError Pos String
 
 instance Show RuntimeError where
@@ -23,37 +41,32 @@ instance Show RuntimeError where
 
 type VM = ExceptT RuntimeError IO
 
-type Index = Int
-type Arity = Int
-type Code  = [Inst]
-data Inst  = App Pos Index Index
-           | Abs Pos Arity Code
-
-type Env   = [Value]
-data Value = Char Word8
-           | Prim (Value -> VM Value)
-           | Clos Env Code
-
+type Code    = [Inst]
+type Env     = [Value]
 data VMState = VMState Code Env [(Code, Env)]
 
+
+-- abstract position in VM
 vmPos :: Pos
 vmPos = Pos "VM"
 
 
-closId :: Value
-closId = Clos [] []
-
+-- boolean values
 closFalse :: Value
 closFalse = Clos [] [Abs vmPos 1 []]
 
 closTrue :: Value
 closTrue = Clos [closId] [Abs vmPos 1 [App vmPos 2 1]]
+    where
+        closId = Clos [] []
 
 
+-- character 'w'
 charLowerW :: Value
 charLowerW = Char 119
 
 
+-- primitive operations
 getWord8 :: IO (Maybe Word8)
 getWord8 = do
     b <- BS.hGet stdin 1
@@ -86,13 +99,8 @@ primSucc = Prim op
         op (Char w) = return $ Char (periodicSucc w)
         op _        = throwError $ RuntimeError vmPos "SUCC: not a character"
 
-initEnv :: Env
-initEnv = [primOut, primSucc, charLowerW, primIn]
 
-initDump :: [(Code, Env)]
-initDump = [([App vmPos 0 0], []), ([], [])]
-
-
+-- evaluation
 envAt :: Pos -> Env -> Int -> VM Value
 envAt pos env i = case env `atMay` i of
     Nothing -> throwError $ RuntimeError pos ("index out of range: " ++ show (i + 1))
@@ -121,7 +129,11 @@ eval (VMState (Abs pos n c' : c) e d)
     | otherwise = eval (VMState c (Clos e [Abs pos (n - 1) c'] : e) d)
 eval _ = throwError $ RuntimeError vmPos "unknown VM state"
 
-run :: Code -> IO (Either RuntimeError Value)
+run :: Code -> IO (Either RuntimeError ())
 run code = do
     let initState = VMState code initEnv initDump
     runExceptT $ eval initState
+    return $ Right ()
+    where
+        initEnv  = [primOut, primSucc, charLowerW, primIn]
+        initDump = [([App vmPos 0 0], []), ([], [])]
