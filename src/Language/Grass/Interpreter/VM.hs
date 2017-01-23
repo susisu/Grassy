0319eeb3aa059cba2f371ef97342ae75ed991a5a
Susisu <susisu2413@yahoo.co.jp>
2017-01-23 13:04:52 +0900

    Rename binder

--- src/Language/Grass/Interpreter/VM.hs
+++ src/Language/Grass/Interpreter/VM.hs
@@ -120,7 +120,7 @@ eval (VMState (App pos m n : c) e d) = do
     a <- envAt pos e n
     case f of
         Char w     -> eval (VMState c (eqChar w a : e) d)
-        Prim p     -> p a >>= \x -> eval (VMState c (x : e) d)
+        Prim op    -> op a >>= \x -> eval (VMState c (x : e) d)
         Clos em cm -> if null c && not (null d)
             then eval (VMState cm (a : em) d)
             else eval (VMState cm (a : em) ((c, e) : d))
