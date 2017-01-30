383778e78abed117f629a5280e0c8a3f01185715
Susisu <susisu2413@yahoo.co.jp>
2017-01-30 13:51:07 +0900

    Implement operations on indexed terms

--- src/Language/Grass/Transpiler/Untyped/Transformation.hs
+++ src/Language/Grass/Transpiler/Untyped/Transformation.hs
@@ -40,3 +40,35 @@ toIndexed ctx (Let _ name x y) = do
     x' <- toIndexed ctx x
     y' <- toIndexed (DefInfo name 1 : ctx) y
     return $ IxLet x' y'
+
+-- operations on indexed terms
+shift :: Int -> Int -> IxTerm -> IxTerm
+shift i n x@(IxVar j)
+    | j >= i    = IxVar (j + n)
+    | otherwise = x
+shift i n (IxAbs x)   = IxAbs (shift (i + 1) n x)
+shift i n (IxApp x y) = IxApp (shift i n x) (shift i n y)
+shift i n (IxLet x y) = IxLet (shift i n x) (shift (i + 1) n y)
+
+subst :: Int -> IxTerm -> IxTerm -> IxTerm
+subst i s x@(IxVar j)
+    | j == i    = s
+    | otherwise = x
+subst i s (IxAbs x)   = IxAbs (subst (i + 1) (shift 0 1 s) x)
+subst i s (IxApp x y) = IxApp (subst i s x) (subst i s y)
+subst i s (IxLet x y) = IxLet (subst i s x) (subst (i + 1) (shift 0 1 s) y)
+
+swap :: Int -> Int -> IxTerm -> IxTerm
+swap i j x@(IxVar k)
+    | i == k    = IxVar j
+    | j == k    = IxVar i
+    | otherwise = x
+swap i j (IxAbs x)   = IxAbs (swap (i + 1) (j + 1) x)
+swap i j (IxApp x y) = IxApp (swap i j x) (swap i j y)
+swap i j (IxLet x y) = IxLet (swap i j x) (swap (i + 1) (j + 1) y)
+
+contains :: IxTerm -> Int -> Bool
+IxVar j   `contains` i = i == j
+IxAbs x   `contains` i = x `contains` (i + 1)
+IxApp x y `contains` i = (x `contains` i) || (y `contains` i)
+IxLet x y `contains` i = (x `contains` i) || (y `contains` (i + 1))
