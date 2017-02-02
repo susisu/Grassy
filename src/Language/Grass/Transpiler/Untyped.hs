4d46120f86bbfea88b3511faa4e9fe83083128b1
Susisu <susisu2413@yahoo.co.jp>
2017-02-02 14:37:38 +0900

    Implement `transpile` function

--- src/Language/Grass/Transpiler/Untyped.hs
+++ src/Language/Grass/Transpiler/Untyped.hs
@@ -1,9 +1,25 @@
+{-# LANGUAGE FlexibleContexts #-}
+
 module Language.Grass.Transpiler.Untyped
-    ( module Language.Grass.Transpiler.Untyped.Term
-    , module Language.Grass.Transpiler.Untyped.Parser
-    , module Language.Grass.Transpiler.Untyped.Transformation
+    ( transpile
     ) where
+
+import Control.Monad.Identity
+import qualified Text.Parsec as P
 
 import Language.Grass.Transpiler.Untyped.Term
 import Language.Grass.Transpiler.Untyped.Parser
 import Language.Grass.Transpiler.Untyped.Transformation
+
+transpile :: P.Stream s Identity Char =>
+       String
+    -> s
+    -> [DefInfo]
+    -> (IxTerm -> IxTerm)
+    -> CharSet
+    -> Either String String
+transpile name src ctx opt cs = case parse name src of
+    Left err   -> Left ("ParseError at " ++ show err)
+    Right defs -> case plant ctx defs opt cs of
+        Left err   -> Left err
+        Right code -> Right code
